2bc1384c510425aa2d330f87a8d92995b1526853
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-17 21:44:00 +0100

    Add Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cards,setCards] = useState([])
+
+  const initGame = () => {
+    const cards = 
+        new Array(16)
+            .fill()
+            .map((_,i) => ({
+                src : `/pix/0${i%8+1}.jpg`,
+                revealed : false,
+            }))
+            .sort(() => Math.random() - .5)
+
+    setCards(cards)
+  }
+
+  useEffect(() => initGame(),[])
   return (
     <div className="App">
-      
+      {cards.map((card,i) => (
+        <div class={`card`}>
+          <div class="card__face card__face--front">
+              <img src={card.src} alt="card-front"  />
+          </div>
+          <div class="card__face card__face--back">
+              <img src="/pix/back.jpg" alt="card-back"/>
+          </div>
+      </div>
+      ))}
+
     </div>
   );
 }
